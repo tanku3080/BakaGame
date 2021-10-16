377a6c090066d66324c596de73b0617783d08940
tanku_for_gitBash <57029371+tanku3080@users.noreply.github.com>
2021-10-16 17:37:26 +0900

    fadeSystemの実装とClear時の音源を入れた

---
+++ まじめなゲーム/Assets/scripts/FadeManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>フェードとシーン切り替えのどちらかを実行する</summary>
+public class FadeManager:MonoBehaviour
+{
+    public enum SCENE_STATUS
+    {
+        START, GAME_PLAY, GAME_OVER, GAME_CLEAR, NONE
+    }
+    public enum FADE_STATUS
+    {
+        FADE_IN, FADE_OUT, AUTO, NONE
+    }
+    /// <summary>フェード処理が終わったかどうかを返す</summary>
+    [HideInInspector] public bool FadeStop { get { return fadeStopFlag; } set { FadeStop = fadeStopFlag; } }
+    private bool fadeStopFlag = false;
+
+    /// <summary>フェード機能のみを行う
+    /// </summary>
+    /// <param name="status">どんなフェードを行いたいか</param>
+    /// <param name="fadeSpeed">Fadeの速度</param>
+    /// <param name="canvas">Fadeさせるオブジェクト</param>
+    public void FadeSystem(FADE_STATUS status = FADE_STATUS.NONE, float fadeSpeed = 0.02f, CanvasGroup canvas = null)
+    {
+        StartCoroutine(StartFadeSystem(status, fadeSpeed, canvas));
+    }
+    private IEnumerator StartFadeSystem(FADE_STATUS _STATUS = FADE_STATUS.NONE, float fadeSpeed = 0.02f, CanvasGroup obj = null)
+    {
+        fadeStopFlag = false;
+        switch (_STATUS)
+        {
+            case FADE_STATUS.FADE_IN:
+                while (true)
+                {
+                    yield return null;
+                    if (obj.alpha >= 1)
+                    {
+                        fadeStopFlag = true;
+                        break;
+                    }
+                    else obj.alpha += fadeSpeed;
+                }
+                break;
+            case FADE_STATUS.FADE_OUT:
+                while (true)
+                {
+                    yield return null;
+                    if (obj.alpha <= 0)
+                    {
+                        fadeStopFlag = true;
+                        break;
+                    }
+                    else obj.alpha -= fadeSpeed;
+                }
+                break;
+            case FADE_STATUS.AUTO:
+                if (obj.alpha == 1)
+                {
+                    FadeSystem(FADE_STATUS.FADE_OUT);
+                }
+                else
+                {
+                    FadeSystem(FADE_STATUS.FADE_IN);
+                }
+                break;
+            case FADE_STATUS.NONE:
+                break;
+        }
+        yield return 0;
+    }
+
+    /// <summary>シーン切り替えのみを行う
+    /// </summary>
+    /// <param name="scene"切り替えたいシーン></param>
+    public void SceneChangeSystem(SCENE_STATUS scene = SCENE_STATUS.NONE)
+    {
+        string changeName = null;
+        switch (scene)
+        {
+            case SCENE_STATUS.START:
+                changeName = "Start";
+                break;
+            case SCENE_STATUS.GAME_PLAY:
+                changeName = "Game";
+                break;
+            case SCENE_STATUS.GAME_OVER:
+                changeName = "GameOver";
+                break;
+            case SCENE_STATUS.GAME_CLEAR:
+                changeName = "GameClear";
+                break;
+            case SCENE_STATUS.NONE:
+                break;
+        }
+        SceneManager.LoadScene(changeName);
+    }
+}
