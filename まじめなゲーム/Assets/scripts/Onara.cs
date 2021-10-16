a6cda8d0c41c044d5810e6e0bb7669358624a2ba
tanku_for_gitBash <57029371+tanku3080@users.noreply.github.com>
2021-10-16 20:09:18 +0900

    おならと移動のスクリプトを追加。メタデータを追加

---
+++ まじめなゲーム/Assets/scripts/Onara.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// おならを出すためのスクリプト
+/// </summary>
+public class Onara : MonoBehaviour
+{
+    [SerializeField] GameObject player = null;
+    [SerializeField] Transform onaraNozzle = null;
+    [SerializeField] float onaraPow = 10000000f;
+    Rigidbody rd = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("おなら");
+            rd.AddForce(onaraPow * Time.deltaTime * Vector3.up,ForceMode.Force);
+        }
+    }
+}
--- まじめなゲーム/Assets/scripts/Player.cs
+++ まじめなゲーム/Assets/scripts/Player.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] GameObject player = null;
+    [SerializeField] float playerSpeed = 100f;
+    Rigidbody rd = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rd = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+        //var mouseH = Input.GetAxis("Mouse H");
+        //var mouseY = Input.GetAxis("Mouse Y");
+
+        if (v != 0)
+        {
+            rd.AddForce((transform.forward * playerSpeed) * v * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (h != 0)
+        {
+            rd.AddForce((transform.right * playerSpeed) * h * Time.deltaTime, ForceMode.VelocityChange);
+        }
+    }
+
+    private void FixedUpdate()
     {
         
     }
