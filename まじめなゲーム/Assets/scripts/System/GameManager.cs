3d44ba7973324001ca4deb30af94a3215ef696d0
tanku_for_gitBash <57029371+tanku3080@users.noreply.github.com>
2021-10-18 22:41:28 +0900

    タイマーのひな型とイベント生成システムのひな型をさくせい

---
+++ まじめなゲーム/Assets/scripts/System/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>タイムリミットになったらTrueにする</summary>
+    public bool timeLimit = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ まじめなゲーム/Assets/scripts/System/PointEventGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>イベント用の物をマップに生成するスクリプト</summary>
+public class PointEventGenerator : MonoBehaviour
+{
+    [SerializeField] List<Transform> eventPoint = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        eventPoint = eventPoint.OrderBy(a => Guid.NewGuid()).ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ここにNPCを生成するように書く
+    }
+}
