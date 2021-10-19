268914f121f9f966fda79c9898e0c9de8f26d7b3
S_Kiyo <kiyo.eva.0913@gmail.com>
2021-10-19 20:27:16 +0900

    カウントダウンタイマー追加

--- まじめなゲーム/Assets/scripts/System/GameManager.cs
+++ まじめなゲーム/Assets/scripts/System/GameManager.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     /// <summary>タイムリミットになったらTrueにする</summary>
     public bool timeLimit = false;
+    public Text timerText;
+    [SerializeField] float totalTime =0;
+    int seconds;
     void Start()
     {
         
@@ -14,6 +18,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        totalTime -= Time.deltaTime;
+        seconds = (int)totalTime;
+        timerText.text = seconds.ToString();
+
+
     }
 }
