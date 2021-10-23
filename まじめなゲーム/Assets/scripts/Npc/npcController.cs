e21af38dbbc7cf0759b17051c5fef9c333dd2689
S_Kiyo <kiyo.eva.0913@gmail.com>
2021-10-23 15:31:51 +0900

    npcController（途中）

--- まじめなゲーム/Assets/scripts/Npc/npcController.cs
+++ まじめなゲーム/Assets/scripts/Npc/npcController.cs
@@ -1,19 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+
 
 public class npcController : MonoBehaviour
 {
 
-    [SerializeField] Animation anime = null;
-    // Start is called before the first frame update
-    void Start()
+
+    //Animation animeition = null;
+
+    Animation anime;
+    
+    public AnimationClip test1;
+    public AnimationClip test2;
+
+    [SerializeField] Animation  anime = null;
+
+    enum STATE
     {
+        Move1,Move2,Move3
     }
 
-    // Update is called once per frame
+    
+    void Start()
+    {
+        anime = GetComponent<Animation>();
+    }
+
+   
     void Update()
+    {
+        switch (test1)
+        {
+            case STATE.Move1:
+                anime = null;
+                break;
+            case STATE.Move2:
+                anime = null;
+                break;
+            case STATE.Move3:
+                anime = null;
+                break;
+        }
+    }
+
+
+    IEnumerator ChangeTest1()
+    {
+        Animation.Stop();
+    }
+
+    void aniSet(STATE st)
     {
         
     }
+
+
 }
