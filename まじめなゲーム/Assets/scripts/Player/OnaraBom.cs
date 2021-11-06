f3e7f39e77a90f5be674712fa12f5fb5a519ff44
WISTLE9999 <yamamoto0903@icloud.com>
2021-11-06 17:48:03 +0900

    bomb

--- まじめなゲーム/Assets/scripts/Player/OnaraBom.cs
+++ まじめなゲーム/Assets/scripts/Player/OnaraBom.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class OnaraBom : MonoBehaviour
 {
     /// <summary>爆発までの時間</summary>
@@ -9,26 +10,34 @@ public class OnaraBom : MonoBehaviour
     [SerializeField] float power = 0;
     [SerializeField] float radius = 0;
     [SerializeField] float upwardsModifier = 0;
-    Rigidbody rb;
+    /// <summary>
+    /// NPCのリスト
+    /// </summary>
+    [SerializeField] List<GameObject> NPC_List;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            Explosion();
-        }
+
     }
 
     private void Explosion()
     {
-        rb.AddExplosionForce(power, new Vector3(0,1,0), radius, upwardsModifier, ForceMode.Impulse);
-        //Destroy(this.gameObject);
-    }
+        foreach (var NPC in NPC_List)
+        {
+            if (Vector3.Distance(this.transform.position, NPC.transform.position) <= radius)
+            {
+                Rigidbody rb;
+                rb = NPC.gameObject.GetComponent<Rigidbody>();
+                rb.AddExplosionForce(power, this.transform.position, radius, upwardsModifier, ForceMode.Impulse);
+            }
+        }        
+        Destroy(this.gameObject);
+    }    
 }
