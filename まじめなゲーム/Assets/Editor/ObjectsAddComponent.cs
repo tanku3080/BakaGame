e811431660d9a30bc513e367914ff779bfd78227
tanku_for_gitBash <57029371+tanku3080@users.noreply.github.com>
2021-11-27 16:14:14 +0900

    エディタ拡張の追加

---
+++ まじめなゲーム/Assets/Editor/ObjectsAddComponent.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ObjectsAddComponent : MonoBehaviour
+{
+    [MenuItem("Editor/AddBLD")]
+
+    //特定のTag(build)を探して指定のコンポーネントをアタッチ
+    private static void AddObj()
+    {
+        foreach (var item in SearchObj())
+        {
+            if (!item.GetComponent<ObjController>())
+            {
+                item.AddComponent<ObjController>();
+            }
+        }
+    }
+    private static GameObject[] SearchObj()
+    {
+        return GameObject.FindGameObjectsWithTag("build");
+    }
+    [MenuItem("Editor/DeletgBLD")]
+    //特定のTagを探して指定のコンポーネントを削除
+    private static void DeletObj()
+    {
+        foreach (var item in SearchObj())
+        {
+            if (item.GetComponent<ObjController>())
+            {
+                DestroyImmediate(item.GetComponent<ObjController>());
+            }
+        }
+    }
+
+    [MenuItem("Editor/AddColision")]
+    private static void AddColision()
+    {
+        foreach (var item in SearchObj())
+        {
+            if (!item.GetComponent<BoxCollider>())
+            {
+                item.AddComponent<BoxCollider>();
+            }
+        }
+    }
+    [MenuItem("Editor/RigitBody/Add")]
+    private static void AddRd()
+    {
+        foreach (var item in SearchObj())
+        {
+            if (!item.GetComponent<Rigidbody>())
+            {
+                item.AddComponent<Rigidbody>();
+            }
+        }
+    }
+    [MenuItem("Editor/RigitBody/Remove")]
+    private static void RemoveSetting()
+    {
+        foreach (var item in SearchObj())
+        {
+            if (item.GetComponent<Rigidbody>())
+            {
+                DestroyImmediate(item.GetComponent<Rigidbody>());
+            }
+        }
+    }
+
+    [MenuItem("Editor/RigitBody/useGravity_false")]
+    private static void Setting()
+    {
+        foreach (var item in SearchObj())
+        {
+            if (!item.GetComponent<Rigidbody>().useGravity == false)
+            {
+                item.GetComponent<Rigidbody>().useGravity = false;
+            }
+        }
+    }
+}
