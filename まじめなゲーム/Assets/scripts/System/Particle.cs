f47e3a4e74e7148e9578616079b4abd4303cfc7a
tanku_for_gitBash <57029371+tanku3080@users.noreply.github.com>
2022-02-04 02:56:03 +0900

    particleを再生する機能を実装。使い方は当該クラスのメソッドを見る。

---
+++ まじめなゲーム/Assets/scripts/System/Particle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>particleを再生する建物に付ける</summary>
+public class Particle : MonoBehaviour
+{
+    ParticleSystem system;
+    // Start is called before the first frame update
+    void Start()
+    {
+        system = gameObject.GetComponent<ParticleSystem>();
+        system.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (system.isStopped)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ まじめなゲーム/Assets/scripts/System/ParticleStart.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ParticleStart : Singleton<ParticleStart>
+{
+    public enum ParticleStatus
+    {
+        ONARA, TOUKAI,JET
+    }
+    /// <summary>
+    /// パーティクルシステムを再生させる
+    /// </summary>
+    /// <param name="origin">再生させる地点場所</param>
+    /// <param name="status">なんの効果か？</param>
+    public void StartParticle(Transform origin, ParticleStatus status)
+    {
+        string name = null;
+        switch (status)
+        {
+            case ParticleStatus.ONARA:
+                name = "Onara_Shooting";
+                break;
+            case ParticleStatus.TOUKAI:
+                name = "Cloud_of_Dust";
+                break;
+            case ParticleStatus.JET:
+                name = "playerOnaraJetParticle";
+                break;
+        }
+        origin = Instantiate((GameObject)Resources.Load($"Prefabs/{name}"), origin.transform.position, Quaternion.identity).transform;
+        origin.parent = transform;
+    }
+}
